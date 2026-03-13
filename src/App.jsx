70ad6b9c21efae3fca215921f290d65ac78d1171
Sonu Kumar <121798722+mahi720@users.noreply.github.com>
2026-03-13 17:36:52 +0530

    Reduce loading timer from 2000ms to 1500ms

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 2000);
+    }, 1500);
 
     return () => clearTimeout(timer);
   }, []);
