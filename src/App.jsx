49f77d7f04d2b16eede8e59794c0f7d59a77a37b
Sonu Kumar <121798722+mahi720@users.noreply.github.com>
2026-03-13 17:41:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 1500);
+    }, 1000);
 
     return () => clearTimeout(timer);
   }, []);
